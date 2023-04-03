98f2f8603b27e56b9fc5b0b840c6238f68652647
Igor-V-Bogdanov <thehogjer@gmail.com>
2023-04-03 21:46:19 +0300

    1st

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const cardsContainer = document.querySelector('.gallery');
+const cardsMarkup = createPhotoGalleryMarkup(galleryItems);
+cardsContainer.insertAdjacentHTML('beforeend', cardsMarkup);
 
+function createPhotoGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+                    <a class="gallery__link" href="${original}">
+                        <img class="gallery__image" src="${preview}" alt="${description}" />
+                    </a>
+              </li>
+            `;
+    })
+    .join('');
+}
+var lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionSelector: 'img',
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 console.log(galleryItems);
