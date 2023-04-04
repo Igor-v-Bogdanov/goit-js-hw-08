c801a667106876a0884472360e0f145cd3a93d9c
Igor-V-Bogdanov <thehogjer@gmail.com>
2023-04-04 13:59:36 +0300

    2nd

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const onPlay = function (data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const time = localStorage.getItem('videoplayer-current-time');
+
+if (time >= 0) {
+  player.setCurrentTime(time);
+} else {
+  player.setCurrentTime(0);
+}
